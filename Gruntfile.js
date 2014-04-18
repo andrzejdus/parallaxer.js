2d16e5480ef45b1412659cf8d2e2340de40ac577
Andrzej Duś <andrzej.dus@gmail.com>
2014-04-18 17:00:07 +0200

    Fixed formatting in grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,59 +19,59 @@
  * @author Andrzej Duś <andrzej@boycoy.com>
  */
 
-module.exports = function(grunt) {
-    // common variables
-    var buildDir = 'dist';
-    var distDir = 'dist';
-    var minifiedFilename = 'parallaxer.min.js';
+module.exports = function (grunt) {
+  // common variables
+  var buildDir = 'dist';
+  var distDir = 'dist';
+  var minifiedFilename = 'parallaxer.min.js';
 
-    // project configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        bump: {
-            options: {},
-            files: [ 'package.json' ]
+  // project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bump: {
+      options: {},
+      files: [ 'package.json' ]
+    },
+    closureBuilder: {
+      options: {
+        closureLibraryPath: 'custom_components/closure-library-20130212-95c19e7f0f5f', // path to closure library
+        inputs: 'src/Parallaxer.js',
+        compilerFile: 'bin/compiler-20130411/compiler.jar', // path to closure compiler
+        compile: true, // boolean
+        compilerOpts: {
+          define: ["'andrzejdus.DEBUG=false'"],
         },
-        closureBuilder: {
-            options: {
-                closureLibraryPath: 'custom_components/closure-library-20130212-95c19e7f0f5f', // path to closure library
-                inputs: 'src/Parallaxer.js',
-                compilerFile: 'bin/compiler-20130411/compiler.jar', // path to closure compiler
-                compile: true, // boolean
-                compilerOpts: {
-                    define: ["'andrzejdus.DEBUG=false'"],
-                },
-                execOpts: {
-                    maxBuffer: 999999 * 1024
-                }
-            },
-            defaultTarget: {
-                src: [
-                    'src',
-                    'custom_components/closure-library-20130212-95c19e7f0f5f/closure',
-                    'custom_components/closure-library-20130212-95c19e7f0f5f/third_party/closure',
-                    'libs/utils.js/src'
-                ],
-                dest: buildDir + '/' + minifiedFilename
-            }
-        },
-        concat: {
-          options: {
-            stripBanners: true,
-            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-              '<%= grunt.template.today("yyyy-mm-dd") %> - <%= pkg.repository.url %> */'
-          },
-          dist: {
-            src: [buildDir + '/' + minifiedFilename],
-            dest: distDir + '/' + minifiedFilename
-          }
+        execOpts: {
+          maxBuffer: 999999 * 1024
         }
-    });
+      },
+      defaultTarget: {
+        src: [
+          'src',
+          'custom_components/closure-library-20130212-95c19e7f0f5f/closure',
+          'custom_components/closure-library-20130212-95c19e7f0f5f/third_party/closure',
+          'libs/utils.js/src'
+        ],
+        dest: buildDir + '/' + minifiedFilename
+      }
+    },
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> - <%= pkg.repository.url %> */'
+      },
+      dist: {
+        src: [buildDir + '/' + minifiedFilename],
+        dest: distDir + '/' + minifiedFilename
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-bumpx');
-    grunt.loadNpmTasks('grunt-closure-tools');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-bumpx');
+  grunt.loadNpmTasks('grunt-closure-tools');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-    // Default task.
-    grunt.registerTask('default', ['bump', 'closureBuilder', 'concat']);
+  // Default task.
+  grunt.registerTask('default', ['bump', 'closureBuilder', 'concat']);
 };
